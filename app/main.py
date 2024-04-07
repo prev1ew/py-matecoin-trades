54ee2255c574b6103c146729470f51942f66f33f
Vitalii <warcied1@gmail.com>
2024-04-07 12:05:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,16 +10,12 @@ def calculate_profit(filename: str) -> None:
         file_data = json.load(file_opened)
     for el in file_data:
         is_it_a_purchase = el["bought"] is not None
-
-        purchased_amount = Decimal(el["bought"]) if is_it_a_purchase \
-            else Decimal(el["sold"]) * -1
-
+        purchased_amount = Decimal(el["bought"]) if is_it_a_purchase else Decimal(el["sold"]) * -1
         money_res += purchased_amount * Decimal(el["matecoin_price"])
         coins_res += purchased_amount
-
     with open("profit.json", "w+") as curr_file:
         obj_to_dump = {
-            "earned_money": f"{money_res: .f}",
-            "matecoin_account": f"{coins_res: .f}"
+            "earned_money": f"{money_res:.7f}",
+            "matecoin_account": f"{coins_res:.5f}"
         }
         json.dump(obj_to_dump, curr_file)
